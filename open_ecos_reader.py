ad2a6686a012014d39466ece29426cf246dd9e68
rkr638@never.com <wjddnd12!@>
2022-08-24 22:55:22 +0900

    튜토리얼 추가, 모듈 변경

--- open_ecos_reader.py
+++ open_ecos_reader.py
@@ -10,7 +10,7 @@ class ECOSReader():
         self.warning = warning
         self.language = language
 
-    def api_call(self, url, api_item, to_num):
+    def _api_call(self, url, api_item, to_num):
         ret = requests.get(url)
         ret_status_code, ret_text = ret.status_code, ret.text
         if ret_status_code != 200:
@@ -30,44 +30,44 @@ class ECOSReader():
             raise ValueError(ret_json)
 
 
-    def satatistic(self, from_num=1, to_num=1000):
+    def satatistic_list(self, from_num=1, to_num=1000):
         api_item = 'StatisticTableList'
         url = f'http://ecos.bok.or.kr/api/{api_item}/{self.apikey}/json/{self.language}/{from_num}/{to_num}/?'
-        return self.api_call(url, api_item, to_num)
+        return self._api_call(url, api_item, to_num)
 
     def statistic_word(self, target_word, from_num=1, to_num=10):
         api_item = 'StatisticWord'
         url = f'http://ecos.bok.or.kr/api/{api_item}/{self.apikey}/json/{self.language}/{from_num}/{to_num}/{target_word}'
-        return self.api_call(url, api_item, to_num)
+        return self._api_call(url, api_item, to_num)
 
-    def statistic_item(self, target_code, from_num=1, to_num=100):
+    def statistic_item_list(self, target_code, from_num=1, to_num=100):
         api_item = 'StatisticItemList'
         url = f'http://ecos.bok.or.kr/api/{api_item}/{self.apikey}/json/{self.language}/{from_num}/{to_num}/{target_code}'
-        return self.api_call(url, api_item, to_num)
+        return self._api_call(url, api_item, to_num)
 
     def statistic_search(self, code, period, from_date, to_date, item_code='?', from_num=1, to_num=100):
         api_item = 'StatisticSearch'
         url = f'http://ecos.bok.or.kr/api/{api_item}/{self.apikey}' \
               f'/json/{self.language}/{from_num}/{to_num}/{code}/{period}/{from_date}/{to_date}/{item_code}'
-        return self.api_call(url, api_item, to_num)
+        return self._api_call(url, api_item, to_num)
 
-    def key_statistic(self, from_num=1, to_num=100):
+    def key_statistic_list(self, from_num=1, to_num=100):
         api_item = 'KeyStatisticList'
         url = f'http://ecos.bok.or.kr/api/{api_item}/{self.apikey}/json/{self.language}/{from_num}/{to_num}'
-        return self.api_call(url, api_item, to_num)
+        return self._api_call(url, api_item, to_num)
 
     def statistic_meta(self, data_name, from_num=1, to_num=100):
         api_item = 'StatisticMeta'
         url = f'http://ecos.bok.or.kr/api/{api_item}/{self.apikey}/json/{self.language}/{from_num}/{to_num}/{data_name}'
-        return self.api_call(url, api_item, to_num)
+        return self._api_call(url, api_item, to_num)
 
 if __name__ == '__main__':
     # test
     er = ECOSReader("sample")
-    print(er.satatistic())
-    print(er.statistic_item('102Y004'))
-    print(er.key_statistic(to_num=10))
-    print(er.key_statistic())
+    print(er.satatistic_list())
+    print(er.statistic_item_list('102Y004'))
+    print(er.key_statistic_list(to_num=10))
+    print(er.key_statistic_list())
     print(er.statistic_meta('경제심리지수'))
     print(er.statistic_search('200Y001', 'A', 2015, 2021))
     print(er.statistic_search('200Y001', 'A', 2015, 2021, '701040203'))
